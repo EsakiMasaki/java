1433aa5b47b1ccf898d22de9b3a7221e6f7d6870
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-02-15 02:42:31 +0000

    calculation

--- index.java
+++ index.java
@@ -1,5 +1,15 @@
 public class Main{
   public static void main (String[] args){
-    System.out.println("Hello World");
+    System.out.println("江﨑です。");
+    System.out.println(24 + "歳です。");
+    System.out.println("電子工作が大好きです。");
+    System.out.println("四則演算をします。");
+    System.out.println(31 + 31);
+    System.out.println(31 - 31);
+    System.out.println(31 * 31);
+    System.out.println(31 / 31);
+    System.out.println(31 % 30);
+    int x = 6;
+    System.out.println(x * x);
   }
 }
