46f6c487128dd0ea7e3be90633073a587ade2251
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-02-22 07:19:09 +0000

    p1_6

---
+++ p1-6.java
@@ -0,0 +1,10 @@
+public class P1_6{
+  public static void main(String[] args){
+    System.out.println("掛け算をします。");
+    System.out.println("1つ目の整数(x)を入力してください。");
+    int x = new java.util.Scanner(System.in).nextInt();
+    System.out.println("2つ目の整数(y)を入力してください。");
+    int y = new java.util.Scanner(System.in).nextInt();
+    System.out.println("x * y = " + x * y);
+  }
+}
