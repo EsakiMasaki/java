5983bb23daa053043f81071c6625cc2e1abe0ae1
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-02-25 04:14:10 +0000

    stady

--- memo.java
+++ memo.java
@@ -4,5 +4,11 @@ public class Memo{
     System.out.println("どうやら疲れているようだ。");
     double heat = 36.0 + new java.util.Random().nextDouble();
     System.out.println("熱を測ると" + (Math.round(heat * 10)) / 10.0 + "だった");
+    heat = (Math.round(heat * 10)) / 10.0;
+    if (heat > 36.7){
+      System.out.println("ねろ");
+    }else{
+      System.out.println("勉強しろ");
+    }
   }
 }
