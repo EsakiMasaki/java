906560a4f8e54a8ee1b467f3a3c873be0364f051
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-03-03 04:16:46 +0000

    questions

--- question.java
+++ question.java
@@ -1,5 +1,7 @@
 public class Question{
   public static void main(String[] args){
-    System.out.println("経歴を中心に自己紹介をしてください。");
+    String[] questions = {"経歴を中心に自己紹介をしてください。","前職に入社した理由はなんですか？"};
+    int rand = new java.util.Random().nextInt(questions.length);
+    System.out.println(questions[rand]);
   }
 }
